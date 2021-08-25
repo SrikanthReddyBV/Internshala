64cad6cce82f64df3f4a9617a12429d7a7ee90a2
krishna <keshri819@gmail.com>
2021-08-26 02:09:59 +0530

    Initial Commit

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect("mongodb://localhost:27017/Book-library", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  });
+  return mongoose.connect(
+    "mongodb+srv://krishna:45K5$U$!gq-C.V2@cluster0.ndxbh.mongodb.net/internshala?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  );
 };
 
 module.exports = connect;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,20 @@
 const express = require("express");
 
-const connection = require("./config/db");
+const path = require("path"); //core module (come inbuilt with node.js) no need to install
+
+const connection = require("./configs/db");
+
+const app = express();
+
+app.use(express.json());
+//joining server.js file with views directory
+
+// views is something which we show to users
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
+
+app.listen("2345", async () => {
+  await connection();
+  console.log("Listening on port 2345");
+});
